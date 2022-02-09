5e41775e050885f2e7cb33ff377fb0051efcbc20
astro <astro@192.168.2.46>
2022-02-09 09:55:32 -0500

    This is only a test

--- newGitSetup/src/newgitsetup/NewGitSetup.java
+++ newGitSetup/src/newgitsetup/NewGitSetup.java
@@ -6,6 +6,7 @@ public class NewGitSetup {
     public static void main(String[] args) {
         System.out.println("This is only a test!");
         System.out.println("This is written on gitHub");
+        System.out.println("This is written using branch");
     }
     
 }
