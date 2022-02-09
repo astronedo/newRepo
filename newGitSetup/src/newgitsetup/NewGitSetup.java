05224c0d48350f0d060b2960e53ac805c02a94cb
astro <astro@192.168.2.46>
2022-02-09 10:01:24 -0500

    This is to be sent to remote changes

--- newGitSetup/src/newgitsetup/NewGitSetup.java
+++ newGitSetup/src/newgitsetup/NewGitSetup.java
@@ -6,6 +6,9 @@ public class NewGitSetup {
     public static void main(String[] args) {
         System.out.println("This is only a test!");
         System.out.println("This is written on gitHub");
+        System.out.println("This is written using branch");
+        System.out.println("This should not be added to the remote repository");
+
     }
     
 }
