598104f472359fafab55f8f321c90ffb96d1a34a
Astronedo <98480105+astronedo@users.noreply.github.com>
2022-02-09 09:47:00 -0500

    Update NewGitSetup.java
    added a message "This is written on gitHub"

--- newGitSetup/src/newgitsetup/NewGitSetup.java
+++ newGitSetup/src/newgitsetup/NewGitSetup.java
@@ -5,6 +5,7 @@ public class NewGitSetup {
 
     public static void main(String[] args) {
         System.out.println("This is only a test!");
+        System.out.println("This is written on gitHub");
     }
     
 }
